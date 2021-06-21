691991dd1649a43b7b419f90d33eb466392739da
Dado <Dado.Bajramovic@zira.com.ba>
2021-06-21 09:43:00 +0200

    change project structure

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router'
-import { FranchiseComponent } from './franchise/franchise.component'
-import { HomeComponent } from './home/home.component'
+import { Routes } from '@angular/router';
 
-export const appRoutes : Routes = [
+import { FranchiseComponent } from './pages/franchise/franchise.component';
+import { HomeComponent } from './pages/home/home.component';
+
+export const appRoutes: Routes = [
     { path: 'franchise', component: FranchiseComponent },
     { path: 'home', component: HomeComponent },
-    { path: '', redirectTo: '/home', pathMatch: 'full'}
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
 ]
