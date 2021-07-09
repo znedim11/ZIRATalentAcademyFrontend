4a11ca7adbb537588f4d599058ec342b6e38e99f
Ajas Zornic <ajas.zornic@edu.fit.ba>
2021-07-09 20:29:49 +0200

    Character: Add, Edit : Update

--- src/app/pages/character/character.module.ts
+++ src/app/pages/character/character.module.ts
@@ -1,24 +1,24 @@
 import { NgModule } from '@angular/core';
 import { QuillModule } from 'ngx-quill';
 import { SharedModule } from '../shared/shared.module';
-import { CharacterGameGridComponent } from './character-overview/character-grid/character-game-grid/character-game-grid.component';
 import { CharacterOverviewComponent } from './character-overview/character-overview.component';
 import { CharacterItemComponent } from './character-search/character-item/character-item.component';
 import { CharacterListComponent } from './character-search/character-list/character-list.component';
 import { CharacterSearchFormComponent } from './character-search/character-search-form/character-search-form.component';
 import { CharacterSearchComponent } from './character-search/character-search.component';
 import { CharacterAddEditComponent } from './character-add-edit/character-add-edit.component';
+import { CharacterGridComponent } from './character-overview/character-grid/character-grid.component';
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   declarations: [
-    CharacterGameGridComponent,
     CharacterOverviewComponent,
     CharacterItemComponent,
     CharacterListComponent,
     CharacterSearchComponent,
     CharacterSearchFormComponent,
     CharacterAddEditComponent,
+    CharacterGridComponent
   ],
   imports: [
     SharedModule,
@@ -26,7 +26,6 @@ import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
     QuillModule.forRoot()
   ],
   exports: [
-    CharacterGameGridComponent,
     CharacterOverviewComponent,
     CharacterItemComponent,
     CharacterListComponent,
